8e5d653e39a17387460399146ba52af0381b684a
айкеновад <@205_8>
2019-04-29 10:49:49 +0600

    Добавьте файлы проекта.

---
+++ Dqueue_(double_queue)_29.04.2019_Lesson/Source.cpp
@@ -0,0 +1,73 @@
+﻿#include"deque.h"
+
+int main() {
+	Deque<double>q;
+	double el;
+
+	int choice;
+	while (true) {
+		cout << "Enter 1 to push back a new element" << endl;
+		cout << "Enter 2 to push front a new element" << endl;
+		cout << "Enter 3 to delete FIRST element" << endl;
+		cout << "Enter 4 to delete LAST element" << endl;
+		cout << "Enter 5 to get last element" << endl;
+		cout << "Enter 6 to get first element" << endl;
+		cout << "Enter 7 to check if the Deque is empty" << endl;
+		cout << "Enter 8 to print all Deque" << endl;
+		cout << "Enter 0 to exit" << endl;
+		cin >> choice;
+		if (choice == 0) break;
+		switch (choice)
+		{
+		case 1:
+			cin >> el;
+			q.push_back(el);
+			break;
+		case 2:
+			cin >> el;
+			q.push_front(el);
+			break;
+		case 3:
+			if (!q.empty())
+				q.pop_front();
+			else cout << "Deque is already empty" << endl << endl;
+			break;
+		case 4:
+			if (!q.empty())
+				q.pop_back();
+			else cout << "Deque is already empty" << endl << endl;
+			break;
+		case 5:
+			if (!q.empty())
+				cout << endl << q.back() << endl;
+			else cout << "Deque is empty" << endl << endl;
+			break;
+		case 6:
+			if (!q.empty())
+				cout << endl << q.front() << endl;
+			else cout << "Deque is empty" << endl << endl;
+			break;
+		case 7:
+			cout << endl;
+			if (q.empty())
+				cout << "Deque is empty now" << endl;
+			else
+				cout << "Deque has still got " << q.size() << " elements" << endl;
+			cout << endl;
+			break;
+		case 8:
+			if (!q.empty())
+				q.print();
+			else cout << "Deque is empty" << endl << endl;
+			break;
+		}
+	}
+
+	Deque<double>q2;
+	q2 = q;
+	cout << "copy:" << endl;
+	q2.print();
+
+	system("pause");
+	return 0;
+}
---
+++ Dqueue_(double_queue)_29.04.2019_Lesson/deque.h
@@ -0,0 +1,199 @@
+﻿#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+//Реализуйте класс Double queue
+// учесть что заполненные ячейки расположены ориентировочно по центру стека
+
+
+template<typename T>
+class Deque {
+private:
+	T* ptr;
+	int begin;
+	int end;
+	int buf_size;
+public:
+	Deque();
+	Deque(int buf_size);
+	Deque(const Deque<T>& obj);
+	~Deque();
+
+	void push_front(T el);
+	void push_back(T el);
+	void pop_front();
+	void pop_back();
+	int size();
+	T& back();
+	T& front();
+	bool empty();
+	void print();
+	Deque<T> operator=(const Deque<T>& obj);
+};
+
+template<typename T>
+inline Deque<T>::Deque()
+{
+	ptr = 0;
+	begin = 0;
+	end = 0;
+	buf_size = 0;
+}
+
+template<typename T>
+inline Deque<T>::Deque(int buf_size)
+{
+	this->buf_size = buf_size;
+	begin = 0;
+	end = 0;
+	ptr = new T[buf_size];
+}
+
+template<typename T>
+inline Deque<T>::Deque(const Deque<T>& obj)
+{
+	buf_size = obj.buf_size;
+	begin = obj.begin;
+	end = obj.end;
+	ptr = new T[buf_size];
+	for (int i = begin; i <= end; i++)
+		ptr[i] = obj.ptr[i];
+}
+
+template<typename T>
+inline Deque<T>::~Deque()
+{
+	delete[] ptr;
+}
+
+template<typename T>
+inline void Deque<T>::push_front(T el)
+{
+	if (buf_size == 0) {
+		buf_size = 10;
+		ptr = new T[buf_size];
+		begin = buf_size / 2 + 1;
+		end = buf_size / 2;
+	}
+	else if (begin == 0) {
+		if (buf_size >= (end - begin) * 2) {
+			T* tmp = new T[buf_size];
+			begin = (buf_size-(end - begin + 1)) / 2;
+			end = (buf_size + (end - begin + 1)) / 2;
+
+			for (int i = begin; i <= end; i++)
+				tmp[i] = ptr[i];
+			delete[] ptr;
+			ptr = tmp;
+		}
+	}
+	else if (buf_size < (end - begin)) {
+		buf_size *= 2;
+		T* tmp = new T[buf_size];
+		begin = (buf_size - (end - begin + 1)) / 2;
+		end = (buf_size + (end - begin + 1)) / 2;
+
+		for (int i = begin; i <= end; i++)
+			tmp[i] = ptr[i];
+
+		delete[] ptr;
+		ptr = tmp;
+	}
+	ptr[--begin] = el;
+}
+
+template<typename T>
+inline void Deque<T>::push_back(T el)
+{
+	if (buf_size == 0) {
+		buf_size = 10;
+		ptr = new T[buf_size];
+		begin = buf_size / 2;
+		end = begin-1;
+	}
+	else if (end == buf_size - 1) {
+		if (buf_size >= (end - begin) * 2) {
+			T* tmp = new T[buf_size];
+			
+			begin = (buf_size - (end - begin + 1)) / 2;
+			end = (buf_size + (end - begin + 1)) / 2;
+
+			for (int i = begin; i <= end; i++)
+				tmp[i] = ptr[i];
+			delete[] ptr;
+			ptr = tmp;
+		}
+	}
+	else if (buf_size < (end - begin)) {
+		buf_size *= 2;
+		T* tmp = new T[buf_size];
+		begin = (buf_size - (end - begin + 1)) / 2;
+		end = (buf_size + (end - begin + 1)) / 2;
+
+		for (int i = begin; i <= end; i++)
+			tmp[i] = ptr[i];
+
+		delete[] ptr;
+		ptr = tmp;
+	}
+	ptr[++end] = el;
+}
+
+template<typename T>
+inline void Deque<T>::pop_front()
+{
+	begin++;
+}
+
+template<typename T>
+inline void Deque<T>::pop_back()
+{
+	end--;
+}
+
+template<typename T>
+inline int Deque<T>::size()
+{
+	return (end - begin + 1);
+}
+
+template<typename T>
+inline T & Deque<T>::back()
+{
+	return ptr[end];
+}
+
+template<typename T>
+inline T & Deque<T>::front()
+{
+	return ptr[begin];
+}
+
+template<typename T>
+inline bool Deque<T>::empty()
+{
+	//return (end - begin == 0);//нельзя потому что, если end==begin значит в els[end] есть элемент, он не пустой
+	return(size() == 0);
+}
+
+template<typename T>
+inline void Deque<T>::print()
+{
+	for (int i = begin; i <= end; i++)
+		cout << ptr[i] << endl;
+	cout << endl;
+}
+
+template<typename T>
+inline Deque<T> Deque<T>::operator=(const Deque<T>& obj)
+{
+	buf_size = obj.buf_size;
+	begin = obj.begin;
+	end = obj.end;
+	ptr = new T[buf_size];
+	for (int i = begin; i <= end; i++)
+		ptr[i] = obj.ptr[i];
+
+	return *this;
+}
